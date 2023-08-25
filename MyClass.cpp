7248ed9f983f0615513b0beffb162161ba656650
mdyasinahmed <yasinahmed7875@gmail.com>
2023-08-25 21:04:04 +0600

    add constructor in cpp

---
+++ MyClass.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class MyClass {     // the class
+    public:
+        MyClass () {    // the constructor
+            cout << "Hello World!";
+        }
+};
+
+int main() {
+    MyClass myObj;
+
+    return 0;
+}

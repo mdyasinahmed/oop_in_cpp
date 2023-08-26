da090bf74d7f7ddc13edc4f038a89420163fcce5
mdyasinahmed <yasinahmed7875@gmail.com>
2023-08-26 23:56:54 +0600

    add namespace code in cpp

---
+++ Namespaces.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+
+// first name space
+namespace first_space {
+    void funct() {
+        cout << "Inside first_space" << endl;
+    }
+}
+
+// second name space
+namespace second_space {
+    void funct() {
+        cout << "Inside second_space" << endl;
+    }
+}
+
+using namespace first_space;
+
+int main() {
+    // This calls the function from the first name space.
+    funct();
+
+    return 0;
+}
